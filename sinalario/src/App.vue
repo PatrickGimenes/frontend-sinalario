4a6940cd8d0e816c96f86b43d925b47699361cef
Patrick Gimenes <48632980+PatrickGimenes@users.noreply.github.com>
2023-09-23 23:52:45 -0300

    fix: duplicating navigation bar and footer

--- sinalario/src/App.vue
+++ sinalario/src/App.vue
@@ -1,19 +1,13 @@
 <template>
   <div id="app">
-    <HomePage />
-
     <RouterView></RouterView>
   </div>
 </template>
 
 <script>
-import HomePage from "./views/HomePage.vue";
-
 export default {
   name: "App",
-  components: {
-    HomePage,
-  },
+  components: {},
 };
 </script>
 
--- sinalario/src/views/LoginPage.vue
+++ sinalario/src/views/LoginPage.vue
@@ -1,44 +1,56 @@
 <template>
-  <main class="form-signin">
-    <form>
-      <img
+  <NavBar />
+  <div class="text-center">
+    <main class="form-signin">
+      <form>
+        <!-- <img
         class="mb-4"
         src="/docs/5.0/assets/brand/bootstrap-logo.svg"
         alt=""
         width="72"
         height="57"
-      />
-      <h1 class="h3 mb-3 fw-normal">Por favor insira suas credenciais</h1>
+      /> -->
+        <h1 class="h3 mb-3 fw-normal">Por favor insira suas credenciais</h1>
 
-      <div class="form-floating">
-        <input
-          type="email"
-          class="form-control"
-          id="floatingInput"
-          placeholder="name@name.com"
-        />
-        <label for="floatingInput">Email</label>
-      </div>
-      <div class="form-floating">
-        <input
-          type="password"
-          class="form-control"
-          id="floatingPassword"
-          placeholder="Password"
-        />
-        <label for="floatingPassword">Senha</label>
-      </div>
+        <div class="form-floating">
+          <input
+            type="email"
+            class="form-control"
+            id="floatingInput"
+            placeholder="name@name.com"
+          />
+          <label for="floatingInput">Email</label>
+        </div>
+        <div class="form-floating">
+          <input
+            type="password"
+            class="form-control"
+            id="floatingPassword"
+            placeholder="Password"
+          />
+          <label for="floatingPassword">Senha</label>
+        </div>
 
-      <div class="checkbox mb-3">
-        <label> <input type="checkbox" value="remember-me" /> Lembre me </label>
-      </div>
-      <button class="w-100 btn btn-lg btn-primary" type="submit">Entrar</button>
-    </form>
-  </main>
+        <div class="checkbox mb-3">
+          <label>
+            <input type="checkbox" value="remember-me" /> Lembre me
+          </label>
+        </div>
+        <button class="w-100 btn btn-lg btn-primary" type="submit">
+          Entrar
+        </button>
+      </form>
+    </main>
+  </div>
+  <FooterBar />
 </template>
 
 <script>
+import FooterBar from "@/components/FooterBar.vue";
+import NavBar from "@/components/NavBar.vue";
+
 export default {
   name: "LoginPage",
+  components: { NavBar, FooterBar },
 };
 </script>
