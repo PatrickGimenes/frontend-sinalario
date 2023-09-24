3400304f1efd20267c2298e856b3a83b81166091
Patrick Gimenes <48632980+PatrickGimenes@users.noreply.github.com>
2023-09-23 23:21:23 -0300

    feat: add navbar and footer component

---
+++ sinalario/src/components/FooterBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="footer mt-auto py-3 bg-light">
+    <div class="container">
+      <span class="text-muted"
+        >Feito com amor por: Nome, Nome, Nome e Nome</span
+      >
+    </div>
+  </footer>
+</template>
+<script>
+export default {
+  name: "FooterBar",
+};
+</script>
---
+++ sinalario/src/views/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <NavBar />
+  <FooterBar />
+</template>
+
+<script>
+import NavBar from "@/components/NavBar.vue";
+import FooterBar from "@/components/FooterBar.vue";
+
+export default {
+  name: "HomePage",
+  components: { NavBar, FooterBar },
+};
+</script>
